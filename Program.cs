899194934039829647c6eb45e7824dc5194bb33f
Denys <denyspavlenko318@gmail.com>
2025-04-26 16:15:18 +0300

    Debug for Railway

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ class Program
     static void Main(string[] args)
     {
         Env.Load();
+        string telegramToken = Environment.GetEnvironmentVariable("TELEGRAMBOT_API_KEY");
+
+        if (string.IsNullOrEmpty(telegramToken))
+        {
+            Console.WriteLine("TELEGRAM TOKEN is missing!");
+        }
+        else
+        {
+            Console.WriteLine("TELEGRAM TOKEN found!");
+        }
         Host tgBot = new Host(Environment.GetEnvironmentVariable("TELEGRAMBOT_API_KEY"), Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
         tgBot.Start();
         Console.ReadLine();
